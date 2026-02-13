d9df680f4fb95252c5aa49803cc1ce99402c0b89
Artem <tarachyovaa@mer.ci.nsu.ru>
2026-02-13 14:11:18 +0700

    task2

--- main/src/main/java/ci/nsu/moble/main/MainActivity.kt
+++ main/src/main/java/ci/nsu/moble/main/MainActivity.kt
@@ -1,18 +1,44 @@
 package ci.nsu.moble.main
 
+//import android.R
+import android.R
 import android.os.Bundle
+import android.util.Log
+import android.widget.EditText
+import android.widget.TextView
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Color.Companion.Green
+import androidx.compose.ui.graphics.Color.Companion.Red
+import androidx.compose.ui.graphics.Color.Companion.Yellow
+import androidx.compose.ui.graphics.Color.Companion.Blue
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import ci.nsu.moble.main.ui.theme.PracticeTheme
 
+private val colorsMap = mapOf(
+    "Red" to Red,
+    "Orange" to Color(0xFFFF5722),
+    "Yellow" to Yellow,
+    "Green" to Green,
+    "Blue" to Blue,
+    "Indigo" to Color(0xFF4B0082),
+    "Violet" to Color(0xFF9C27B0),
+
+)
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -30,12 +56,19 @@ class MainActivity : ComponentActivity() {
     }
 }
 
+
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+    Button(onClick = {
+        Log.w("test","Мы кликнули")
+    }, colors = ButtonDefaults.buttonColors(containerColor = Color.Green), modifier = modifier.width(500.dp).height(60.dp)) {
+        Text("Применить цвет")
+
+    }
+    TextField(value = String ){
+
+    }
+
 }
 
 @Preview(showBackground = true)
